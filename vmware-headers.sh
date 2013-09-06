6788ef90c8cbc69ea34c8e695b0d82ec2d6076b4
Peter Oostewechel <peter_oostewechel@hotmail.com>
2013-09-06 15:37:34 +0200

    Added vmware fix header script for vmware 9 on linux 3.8+ kernels

---
+++ vmware-headers.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo ln -s /usr/src/linux-headers-$(uname -r)/include/generated/uapi/linux/version.h /usr/src/linux-headers-$(uname -r)/include/linux/version.h
+sudo vmware-modconfig --console --install-all
